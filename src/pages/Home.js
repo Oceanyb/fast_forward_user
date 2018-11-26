94da165587c433ef659dd6b8056f2dcac329a87c
Oceanyb <2046268861@qq.com>
2018-11-26 19:29:03 +0800

    start

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { NavBar } from 'antd-mobile';
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <NavBar>首 页</NavBar>
+    )
+  }
+}
+
+export default Home
